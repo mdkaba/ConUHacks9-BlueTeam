26fceb03a94987a43e0ea30da5a97b033322bebc
TeddyGuy <m.teddy.guy@proton.me>
2025-02-01 17:48:16 -0500

    add dash to ship

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -1,12 +1,20 @@
 extends Sprite2D
 
 var angular_speed = PI
+var dash_cooldown: float = 2
+var current_dash_cooldown: float = 0
+var dash_duration: float = 0.1
+var current_dash_duration: float = 0
 
 func _init() -> void:
 	print("Started")
 	
 func _process(delta: float) -> void:
-	var speed = 500
+	process_dash(delta)
+		
+	var speed = 1000
+	if(current_dash_duration > 0):
+		speed = 3500
 	var direction = 0
 	if Input.is_key_pressed(KEY_D):
 		direction = 1
@@ -16,11 +24,29 @@ func _process(delta: float) -> void:
 	rotation += angular_speed * delta * direction
 	
 	var velocity = Vector2.ZERO
-	if Input.is_key_pressed(KEY_SPACE):
-		speed = 800
+	
 	if Input.is_key_pressed(KEY_W):
 		velocity = Vector2.UP.rotated(rotation) * speed
-
-
+	if Input.is_key_pressed(KEY_SPACE):
+			if(current_dash_cooldown == 0):
+				current_dash_cooldown = dash_cooldown
+				current_dash_duration = dash_duration
 	position += velocity * delta
+	
+	
+	
+func process_dash(delta: float) -> void:
+	if (current_dash_duration > 0):
+		current_dash_duration -= delta
+		if(current_dash_duration < 0):
+			current_dash_duration = 0
+		return
+		
+	if(current_dash_cooldown > 0):
+		current_dash_cooldown -= delta
+		if(current_dash_cooldown < 0):
+			current_dash_cooldown = 0
+		print(current_dash_cooldown)
+	
+
 	
